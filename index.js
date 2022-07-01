9489aa7d0a6ca5f51aedd122244d3f1869593392
Daniel Susman <frenchminister@gmail.com>
2022-07-01 14:01:22 -0400

    let's try to deploy again

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ document.getElementById('theme-toggle').addEventListener('click', (e) => {
   const checked = e.target.checked;
   document.body.setAttribute('theme', checked ? 'dark' : 'light');
 });
+
