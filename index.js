ee8396a0e84ff99adabd1ef5c7ed87cb7c6e3110
Daniel Susman <frenchminister@gmail.com>
2022-07-01 14:57:55 -0400

    Remove dark mode toggle

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-document.getElementById('theme-toggle').addEventListener('click', (e) => {
+document.getElementById("theme-toggle").addEventListener("click", (e) => {
   const checked = e.target.checked;
-  document.body.setAttribute('theme', checked ? 'dark' : 'light');
+  document.body.setAttribute("theme", checked ? "dark" : "light");
 });
-
